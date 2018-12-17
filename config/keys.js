71daf1440dfba14f8547402e75c0a09d395aa843
Digital Villa <villanueva1077@gmail.com>
2018-12-17 13:44:09 -0700

    connected to Mongo DB

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-     mongoURI: "mongodb://villa:villa1212@ds159110.mlab.com:59110/first_db"
+     mongoURI: "mongodb://villa:villa1212@ds159110.mlab.com:59110/first_db",
+     customKey: "ED3F29FEE9E28E36F7CDC8115E0642E9"
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,35 +1,107 @@
 // authentication credentials
 const express = require('express');
 const router = express.Router();
-// Load user model
 const User = require('../../models/User');
+const bcrypt = require('bcryptjs')
+const gravatar = require('gravatar');
+const jwt = require('jsonwebtoken');
+const key = require('../../config/keys').customKey;
+
 
 /**
  * @route GET api/users/test
  * @desc test for the route
  * @access Public 
  */
-router.get('/test', (req,res) => res.json({msg:"Users is running"}));
+router.get('/test', (req, res) => res.json({
+    msg: "Users is running"
+}));
 
 /**
  * @route GET api/users/register
  * @desc  Register new user
  * @access Public 
  */
-router.post('/register', (req,res) => {
-    User.findOne({email: req.body.email})
+router.post('/register', (req, res) => {
+    User.findOne({email: req.body.email })
+        .then(user => {
+            if (user) // if users exist warn the sender
+                return res.status(400).json({
+                    email: "Email already exists"
+                });
+
+            const avatar = gravatar.url(req.body.email, {
+                s: '200', //size
+                r: 'pg', //rating
+                d: 'mm' //default
+            });
+
+            // create user
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password,
+                avatar
+            })
+
+            // encrypt password
+            //  https://medium.com/@paulrohan/how-bcryptjs-works-90ef4cb85bf4
+            bcrypt.genSalt(10, (err, salt) => {
+                if (err) throw err;
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                        .then((user) => res.json(user))
+                        .catch((err) => console.log(err))
+                })
+            })
+
+        })
+});
+
+
+/**
+ * @route GET api/users/login
+ * @desc  Login user / send JWT token
+ * @access Public 
+ */
+
+ router.post('/login', (req,res ) => {
+     //get params
+     const email = req.body.email;
+     const password = req.body.password;
+    // find user in DB 
+    User.findOne({email})
     .then(user => {
-        if(user)  // if users exist warn the sender
-            return res.status(400).json({email: "Email already exists"});
-        const newUser = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-            avatar
+        //check user
+        if (!user)
+            return res.status(404).json({email: "User not found"});
+        
+        //check password
+        bcrypt.compare(password, user.password ) 
+        .then(isMatch => {
+            if (!isMatch) 
+                return res.status(400).json({password: "Invalid password"});
+            
+            //content to return with jwt
+            const payload = {id: user.id, name: user.name, avatar: user.avatar}
+           
+                // users matched return web token for secure route jwt
+            jwt.sign(payload, key, {expiresIn: 3600}, (err,token) => {
+                res.json({
+                    success: true,
+                    token: 'Bearer '+token
+                })
+
+            });
+
+            
+
         })
 
     })
-} );
 
+ })
 
 module.exports = router;
